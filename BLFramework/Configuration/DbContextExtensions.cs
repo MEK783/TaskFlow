f391f98a5525905762a7ac964dc8e490aef4db2b
MEK783 <mark.e.farrugia@gmail.com>
2026-02-09 18:16:02 +0100

    Enhanced the SQL connection retry mechanism in BLFramework
    Since the production site database runs on an Azure SQL database that can pause due to inactivity, the retry mechanism had to be enhanced.  This was done by:
    * Increasing the maximum retry count to 10
    * Added a maximum delay between retries to 30 seconds
    * Ensdured that all transient errors fired by Azure DB, especially 40613, are included in the error number list that triggers a retry
    * Lenghtened the command timeout up to 90 seconds
    
    Noticed that the auto deployment to the Azure Database was failing due to some inconsistencies in the SQL project definition. These changes were reverted

--- BLFramework/Configuration/DbContextExtensions.cs
+++ BLFramework/Configuration/DbContextExtensions.cs
@@ -22,8 +22,13 @@ namespace BLFramework.Configuration
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(connectionString, sqlOptions =>
                 {
-                    sqlOptions.EnableRetryOnFailure(maxRetryCount: 5);
-                    sqlOptions.CommandTimeout(30);
+                    // Enable retry on failure for transient faults in Azure SQL due to database pausing
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 10, // Maximum number of retry attempts
+                        maxRetryDelay: TimeSpan.FromSeconds(30), // Maximum delay between retries
+                        errorNumbersToAdd: new[] { 4060, 10928, 10929, 40197, 40501, 40613 } // SQL error codes for transient faults in Azure SQL
+                        );
+                    sqlOptions.CommandTimeout(90);
                 }));
 
             return services;
